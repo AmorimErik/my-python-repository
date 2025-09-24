15f1193976eab83faccaa3a0c0af4f6f145f944e
AmorimErik <erik.benvindo@gmail.com>
2025-09-24 14:54:31 -0300

    Código inicial de utilização da biblioteca Streamlit

---
+++ main.py
@@ -0,0 +1,43 @@
+# Importar as bibliotecasa
+import streamlit as st
+import pandas as pd
+import yfinance as yf
+from datetime import datetime
+
+# Criar as funções de carregamento de dados
+# Cotações de do Itaú - ITUB4 - 2010 a 2024
+
+
+@st.cache_data
+def carregar_dados(empresas):
+    dados = pd.DataFrame()
+    for ticker in empresas:
+        acao = yf.Ticker(ticker)
+        cotacoes = acao.history(start="2023-09-01", end=datetime.today())
+        if not cotacoes.empty:
+            dados[ticker] = cotacoes["Close"]
+    return dados
+
+
+st.write(
+    """
+        # App Preço de Ações
+        O gráfico abaixo representa a evolução do preço das ações ao longo dos anos"""
+)
+
+acoes = ["ITUB4.SA", "PETR4.SA", "MGLU3.SA", "VALE3.SA", "ABEV3.SA", "GGBR4.SA"]
+
+dados = carregar_dados(acoes)
+lista_acoes = st.multiselect("Escolha as ações para visualizar", dados.columns)
+
+if lista_acoes:
+    dados = dados[lista_acoes]
+    if len(lista_acoes) == 1:
+        acao_unica = lista_acoes[0]
+        dados = dados.rename(columns={acao_unica: "Close"})
+
+
+print(dados)
+
+# # Cria o gráfico
+st.line_chart(dados)
