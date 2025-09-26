5ba79b2c4648dac4e64eb282d11692401562cc13
AmorimErik <erik.benvindo@gmail.com>
2025-09-25 21:02:25 -0300

    Atualiza de código com inclusão de novas funções.

--- main.py
+++ main.py
@@ -5,9 +5,6 @@ import yfinance as yf
 from datetime import datetime, timedelta
 
 # Criar as funções de carregamento de dados
-# Cotações de do Itaú - ITUB4 - 2010 a 2024
-
-
 @st.cache_data
 def carregar_dados(empresas):
     dados = pd.DataFrame()
@@ -19,33 +16,95 @@ def carregar_dados(empresas):
     return dados
 
 
+@st.cache_data
+def carregar_tickers_acoes():
+    base_tickers = pd.read_csv("IBOV.csv", sep=";")
+    tickers = list(base_tickers["Código"])
+    tickers = [item + ".SA" for item in tickers]
+    return tickers
+
+
+acoes = carregar_tickers_acoes()
+dados = carregar_dados(acoes)
+
+# Cria a interface do Streamlit
 st.write(
     """
         # App Preço de Ações
-        O gráfico abaixo representa a evolução do preço das ações ao longo dos anos"""
+        O gráfico abaixo representa a evolução do preço das ações no período"""
 )
-
-acoes = ["ITUB4.SA", "PETR4.SA", "MGLU3.SA", "VALE3.SA", "ABEV3.SA", "GGBR4.SA"]
-
-dados = carregar_dados(acoes)
-lista_acoes = st.sidebar.multiselect("Escolha as ações para visualizar", dados.columns)
-
+# Barra lateral com o filtro
 st.sidebar.header("Filtros")
 
+lista_acoes = st.sidebar.multiselect("Escolha as ações para visualizar", dados.columns)
 
-#  Filtra as acoes
 if lista_acoes:
     dados = dados[lista_acoes]
     if len(lista_acoes) == 1:
         acao_unica = lista_acoes[0]
         dados = dados.rename(columns={acao_unica: "Close"})
 
-# Filtra as datas
+# Filtro datas
 data_inicial = dados.index.min().to_pydatetime()
 data_final = dados.index.max().to_pydatetime()
-filtro_data = st.sidebar.slider("Selecione o período", min_value=data_inicial, max_value=data_final, value=(data_inicial, data_final), step=timedelta(days=1))
+periodo = st.sidebar.slider(
+    "Selecione o período para visualização",
+    min_value=data_inicial,
+    max_value=data_final,
+    value=(data_inicial, data_final),
+    step=timedelta(days=1),
+)
 
-dados = dados.loc[filtro_data[0]:filtro_data[1]]
+dados = dados.loc[periodo[0] : periodo[1]]
 
-# # Cria o gráfico
+# Cria o gráfico
 st.line_chart(dados)
+
+msg_performance = ""
+
+if len(lista_acoes) == 0:
+    lista_acoes = list(dados.columns)
+elif len(lista_acoes) == 1:
+    dados = dados.rename(columns={"Close": acao_unica})
+
+carteira = [1000 for acao in lista_acoes]
+saldo_inicial = sum(carteira)
+
+for i, acao in enumerate(lista_acoes):
+    performance = dados[acao].iloc[-1] / dados[acao].iloc[0] - 1
+    performance = float(performance)
+
+    carteira[i] = carteira[i] * (performance + 1)
+
+    # Colando cor texto
+    if performance > 0:
+        msg_performance = msg_performance + f"  \n{acao}: :green[{performance:.1%}]"
+    elif performance < 0:
+        msg_performance = msg_performance + f"  \n{acao}: :red[{performance:.1%}]"
+    else:
+        msg_performance = msg_performance + f"  \n{acao}: {performance:.1%}"
+
+saldo_final = sum(carteira)
+perf_carteira = saldo_final / saldo_inicial - 1
+
+if perf_carteira > 0:
+    msg_perf_carteira = (
+        f"Performance da carteira com todos os ativos: :green[{perf_carteira:.1%}]"
+    )
+elif perf_carteira < 0:
+    msg_perf_carteira = (
+        f"Performance da carteira com todos os ativos: :red[{perf_carteira:.1%}]"
+    )
+else:
+    msg_perf_carteira = (
+        f"Performance da carteira com todos os ativos: {perf_carteira:.1%}"
+    )
+
+st.write(
+    f"""
+        ### Performance dos Ativos
+        Essa foi a performance de cada ativo no período selecionado:
+        {msg_performance}
+        \n{msg_perf_carteira}
+        """
+)
